b5e540a6270fc038a38b613b66842e824b1a7ea8
karikala <kalarajinikanth@gmail.com>
2021-10-12 17:24:31 +0530

    final

--- reduced_form.py
+++ reduced_form.py
@@ -1,53 +1,37 @@
-def check_prime(n):
+def main():
+    F = float(input("Enter a number"))
+    l = len(str(F)) + 1
     count = 0
-    for i in range(2, int(n / 2) + 1):
-        if i > 2:
-            if n % i == 0:
-                count += 1
-    if count == 0:
-        return True
-    else:
-        return False
+    for i in range(l + 1):
+        if not isinstance(F, int):
+            if F % 1 != 0:
+                F = F * 10
+                count = count + 1
+
+    num = int(F)
+    denom = 10 ** count
+    return num, denom
 
 
-def reduced_form(numer, denomi, Minimum):
-    flag = 0
-    if numer == 1 or denomi == 1:
-        print(str(numer) + " / " + str(denomi))
-        flag = 1
-
-    for j in range(2, Minimum):
-        if denomi % j == 0 and numer % j == 0:
-            denomi = denomi // j
-            numer = numer // j
-            min = numer if numer < denomi else denomi
-            reduced_form(numer, denomi, min)
-            if check_prime(numer) and check_prime(denomi):
-                print(str(numer) + " / " + str(denomi))
-                flag = 1
-
-        if flag == 1:
-            return
+def _gcd(x, y):
+    if x == 0:
+        return y
+    if y == 0:
+        return x
+    if x == y:
+        return x
+    if x > y:
+        return _gcd(x - y, y)
+    return _gcd(y, y - x)
 
 
-F = float(input("Enter a number"))
-l = len(str(F)) + 1
-count = 0
-for i in range(l + 1):
-    if not isinstance(F, int):
-        if F % 1 != 0:
-            F = F * 10
-            count = count + 1
+def reduced_form(x, y):
+    z = _gcd(x, y)
+    up = x // z
+    down = y // z
+    return str(up) + "/" + str(down)
 
-numerator = int(F)
-denominator = 10 ** count
 
-print(numerator, denominator)
-if numerator > denominator:
-    M = denominator
-else:
-    M = numerator
-
-print(False and False)
-
-reduced_form(numerator, denominator, M)
+if __name__ == "__main__":
+    x, y = main()
+    print(reduced_form(x, y))
